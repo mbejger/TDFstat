1813a3bd0a289bc94542924e38d2305b53571d68
Paweł Ciecieląg <pciecielag@gmail.com>
2025-05-15 12:03:12 +0200

    [search] fixed main.c and timer.c indentation

--- search/network/openmp/main.c
+++ search/network/openmp/main.c
@@ -32,109 +32,106 @@ volatile sig_atomic_t save_state = 0;
 
 int main (int argc, char* argv[]) {
 
-  Command_line_opts opts;
-  Search_settings sett;
-  Search_range s_range; 
-  Aux_arrays aux_arr;
-  int i; 
+     Command_line_opts opts;
+     Search_settings sett;
+     Search_range s_range;
+     Aux_arrays aux_arr;
+     int i;
 
 #define QUOTE(x) #x
 #define STR(macro) QUOTE(macro)
 #define CVSTR STR(CODEVER)
 
-  printf("Code version : " CVSTR "\n");
-  if (signal(SIGUSR1, sig_handler) != SIG_ERR &&
-      signal(SIGTERM, sig_handler) != SIG_ERR )
-    printf("State saved on SIGTERM or SIGUSR1\n");    
+     printf("Code version : " CVSTR "\n");
+     if (signal(SIGUSR1, sig_handler) != SIG_ERR &&
+          signal(SIGTERM, sig_handler) != SIG_ERR )
+     printf("State saved on SIGTERM or SIGUSR1\n");
 
-  // Command line options 
-  read_ini_file(&sett, &opts, argc, argv);    
-	
-  // Output data handling
-  struct stat buffer;
+     // Command line options
+     read_ini_file(&sett, &opts, argc, argv);
 
-  if (stat(opts.outdir, &buffer) == -1) {
-    if (errno == ENOENT) {
-      // Output directory apparently does not exist, try to create one
-      if(mkdir(opts.outdir, S_IRWXU | S_IRGRP | S_IXGRP 
-          | S_IROTH	| S_IXOTH) == -1) {
-	      perror (opts.outdir);
-	      return 1;
-      }
-    } else { // can't access output directory
-      perror (opts.outdir);
-      return 1;
-    }
-  }
- 
-  // Detector network settings
-  detectors_settings(&sett, &opts); 
+     // Output data handling
+     struct stat buffer;
 
-  // Grid data 
-  read_grid(&sett, &opts);	
+     if (stat(opts.outdir, &buffer) == -1) {
+          if (errno == ENOENT) {
+               // Output directory apparently does not exist, try to create one
+               if(mkdir(opts.outdir, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH	| S_IXOTH) == -1) {
+                    perror (opts.outdir);
+                    return 1;
+               }
+          } else { // can't access output directory
+               perror (opts.outdir);
+               return 1;
+          }
+     }
 
-  // Search settings
-  search_settings(&sett); 
+     // Detector network settings
+     detectors_settings(&sett, &opts);
 
-  // Array initialization and reading the ephemerids 
-  init_arrays(&sett, &opts, &aux_arr);
+     // Grid data
+     read_grid(&sett, &opts);
 
-  // Narrowing-down the band (excluding the edges 
-  // according to the opts.narrowdown parameter)
-  // adds two lines
-  if(opts.narrowdown < 0.5*M_PI) narrow_down_band(&sett, &opts);
-  
-  // Reading veto lines data from external files
-  printf("Reading veto files...\n");
-  read_lines(&sett, &opts);
-  if (opts.gen_vlines_flag) exit(EXIT_SUCCESS);
+     // Search settings
+     search_settings(&sett);
 
-  // Amplitude modulation functions for each detector  
-  for(i=0; i<sett.nifo; i++)   
-    rogcvir(&ifo[i]); 
+     // Array initialization and reading the ephemerids
+     init_arrays(&sett, &opts, &aux_arr);
 
-  // Grid search range
-  if(strlen(opts.addsig)) { 
-    // If addsig switch used, add signal from file, 
-    // search around this position (+- gsize)
-    add_signal(&sett, &opts, &aux_arr, &s_range); 
-  } else 
-    // Set search range from range file  
-    set_search_range(&sett, &opts, &s_range);
+     // Narrowing-down the band (excluding the edges
+     // according to the opts.narrowdown parameter)
+     // adds two lines
+     if(opts.narrowdown < 0.5*M_PI) narrow_down_band(&sett, &opts);
 
-  // FFT plans 
-  FFTW_plans fftw_plans;
-  FFTW_arrays fftw_arr;
-  plan_fftw(&sett, &opts, &fftw_plans, &fftw_arr, &aux_arr);
+     // Reading veto lines data from external files
+     printf("Reading veto files...\n");
+     read_lines(&sett, &opts);
+     if (opts.gen_vlines_flag) exit(EXIT_SUCCESS);
 
-  // Checkpointing
-  int Fnum=0;	// candidate signal number
-  read_checkpoints(&opts, &s_range, &Fnum);
+     // Amplitude modulation functions for each detector
+     for(i=0; i<sett.nifo; i++)
+          rogcvir(&ifo[i]);
 
-  // main search job
-  search(&sett, &opts, &s_range, 
-         &fftw_plans, &fftw_arr, &aux_arr,
-	 &Fnum);
+     // Grid search range
+     if(strlen(opts.addsig)) {
+          // If addsig switch used, add signal from file,
+          // search around this position (+- gsize)
+          add_signal(&sett, &opts, &aux_arr, &s_range);
+     } else
+     // Set search range from range file
+     set_search_range(&sett, &opts, &s_range);
 
-  // state file is emptied and closed in jobcore to mark successful end
-  // do not remove it here
-  /*
-  FILE *state;
-  if(opts.checkp_flag) {
-    remove(opts.qname);
-  }
-  */
-  
-  // Cleanup & memory free 
-  cleanup(&sett, &opts, &s_range, &fftw_plans, &fftw_arr, &aux_arr);
+     // FFT plans
+     FFTW_plans fftw_plans;
+     FFTW_arrays fftw_arr;
+     plan_fftw(&sett, &opts, &fftw_plans, &fftw_arr, &aux_arr);
 
-  return 0; 
-	
+     // Checkpointing
+     int Fnum=0;	// candidate signal number
+     read_checkpoints(&opts, &s_range, &Fnum);
+
+     // main search job
+     search(&sett, &opts, &s_range, &fftw_plans, &fftw_arr, &aux_arr, &Fnum);
+
+     // state file is emptied and closed in jobcore to mark successful end
+     // do not remove it here
+     /*
+     FILE *state;
+     if(opts.checkp_flag) {
+     remove(opts.qname);
+     }
+     */
+
+     // Cleanup & memory free
+     cleanup(&sett, &opts, &s_range, &fftw_plans, &fftw_arr, &aux_arr);
+
+     return 0;
+
 }
 
 
 // signal handler to save state and exit before end
 static void sig_handler(int signo)
 {
-  if (signo == SIGTERM || signo == SIGUSR1) save_state = 1;
+     if (signo == SIGTERM || signo == SIGUSR1) save_state = 1;
 }
--- search/network/openmp/timer.c
+++ search/network/openmp/timer.c
@@ -2,12 +2,12 @@
 #include "timer.h"
 
 struct timespec get_current_time(clockid_t cid) {
-  struct timespec t;
-  // CLOCK_PROCESS_CPUTIME_ID or CLOCK_REALTIME
-  clock_gettime(cid, &t);
-  return t;
+     struct timespec t;
+     // CLOCK_PROCESS_CPUTIME_ID or CLOCK_REALTIME
+     clock_gettime(cid, &t);
+     return t;
 }
 
 double get_time_difference(struct timespec t0, struct timespec t1) { // in s
-  return (t1.tv_sec-t0.tv_sec)+(double)(t1.tv_nsec-t0.tv_nsec)/(double)NANO_INV;
+     return (t1.tv_sec-t0.tv_sec)+(double)(t1.tv_nsec-t0.tv_nsec)/(double)NANO_INV;
 }
