5b5ed44a778866fbfa42f78d9f1baa4310bad53b
Michal Bejger <michal.bejger@ligo.org>
2025-06-23 15:02:14 +0200

    main.c cleanup

--- search/network/openmp/main.c
+++ search/network/openmp/main.c
@@ -90,31 +90,6 @@ int main (int argc, char* argv[]) {
   // Search settings
   search_settings(&sett); 
 
-  if (opts.is_directed) {
-    
-    double be[2]; 
-    int hemisphere; 
-
-    hemisphere = ast2lin(opts.ra_val, opts.dec_val, C_EPSMA, be);
- 
-    printf("Hemisphere: %d, be[0]: %lf, be[1]: %lf\n", hemisphere, be[0], be[1]);
-
-    //be = al/sett->oms;
-
-    double D, nn, mm, al1, al2;
-    al1 = be[0]*sett.oms;
-    al2 = be[1]*sett.oms;
-
-    D = sett.M[10]*sett.M[15] - sett.M[11]*sett.M[14];
-    nn = (al1*sett.M[15] - al2*sett.M[14])/D; 
-    mm = (al2*sett.M[10] - al1*sett.M[11])/D;
-
-    printf("nn: %lf, mm: %lf\n", nn, mm);
-
-    exit(0);
-  } 
-  
-
   // Array initialization, reading in the input data and the ephemerids 
   init_arrays(&sett, &opts, &aux_arr);
 
